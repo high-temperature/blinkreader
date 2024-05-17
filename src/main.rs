95a9f34ff27aee51511ccb27a40a21752a974046
kagakuma <yudzuru.tokioka.0824@gmail.com>
2024-05-18 01:41:35 +0900

    anti aliasing

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ mod blink_reader;
 mod message;
 
 use blink_reader::BlinkReader;
-use iced::Application;
+use iced::{Application, Settings};
 
 fn main() -> iced::Result {
-    BlinkReader::run(Default::default())
+    BlinkReader::run(Settings{
+        antialiasing: true,
+        ..Settings::default()
+    })
 }
